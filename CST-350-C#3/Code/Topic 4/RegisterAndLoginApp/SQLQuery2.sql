cacac7e8bc0e6d0ef13b219f7a7380d8e7dfd230
omniV1 <owen.lindsey98@outlook.com>
2025-10-19 21:23:21 -0700

    keeping up to speed

--- CST-350-C#3/Code/Topic 4/RegisterAndLoginApp/SQLQuery2.sql
+++ CST-350-C#3/Code/Topic 4/RegisterAndLoginApp/SQLQuery2.sql
@@ -1,1 +1,3 @@
-﻿INSERT INTO dbo.UserAccount (Username, MyPassword, GroupName) VALUES ('user1', 'password1', 'group1');
+version https://git-lfs.github.com/spec/v1
+oid sha256:23e8313a9ca092feb24d21de3907ab4b53830a7199f967fb3ca5cfa4e012090f
+size 105
